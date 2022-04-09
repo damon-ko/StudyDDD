c8d3fb4eac45420ac3eef13d4e10f189ed25a4c1
damon.ko <damon.kkodae@gmail.com>
2022-04-09 15:31:13 +0900

    #1 chapter1 학습하기

---
+++ src/main/java/com/study/ddd/order/Money.java
@@ -0,0 +1,2 @@
+package com.study.ddd.order;public class Money {
+}
---
+++ src/main/java/com/study/ddd/order/Order.java
@@ -0,0 +1,6 @@
+package com.study.ddd;
+
+public class Order {
+    public void chaingeShipped() {}
+    public void changeShippingInfo() {}
+}
